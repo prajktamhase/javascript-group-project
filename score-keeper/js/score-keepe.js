d2479f59610eaa77e531f287ef461426ce59b741
Harshali Kanoje <harshalikanoje@gmail.com>
2023-08-26 12:52:27 -0700

    css are added to index.html

--- score-keeper/js/score-keepe.js
+++ score-keeper/js/score-keepe.js
@@ -5,15 +5,22 @@ function increaseScore(ind)
     const scoreElement = document.getElementById(`score${ind}`);
     const scoreValue = parseInt( scoreElement.innerText);
     scoreElement.innerText = scoreValue +1;
+    if(scoreElement.innerText == 11)
+    {
+        alert("Awesome! You are winner");
+    }
 }
 
 function decreaseScore(ind)
 {
+    
     const scoreElement = document.getElementById(`score${ind}`);
     const scoreValue = parseInt( scoreElement.innerText);
+    if(scoreElement.innerText == 0)
+    {
+        alert("Oop! Try your best");
+       return;
+    }
     scoreElement.innerText = scoreValue -1;
-    // if(scoreElement.innerText == 0)
-    // {
-    //     return;
-    // }
+    
 }
